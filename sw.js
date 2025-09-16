191933218056714c87f829ed3e3a473322c5316d
samirrhashimov <samirhasimov10t@gmail.com>
2025-09-16 22:49:48 +0400

    1.5.8 (PWA)

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = "whispen-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/styles/styles.css",
+  "/scripts/script.js",
+  "/pdf.js/web/viewer.html",
+  "/pdf.js/web/styles.html",
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
